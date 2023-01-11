11602ac4b0b8d7c0ad7136b155496f7db1923ff2
poss03251 <mpossamaim@gmail.com>
2023-01-11 11:00:49 +0000

    added files to avoid error docker and task to save

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, unicode_literals
+from django_redis import get_redis_connection
 
 from celery import shared_task
 
 @shared_task
-def add(x, y):
-    return x + y
+def save_user_token_to_redis(user: int, token: str):
+    
+    redis = get_redis_connection()
+    redis.set(f'user:{user}:token', token, ex=60 * 60 * 24 * 7)
