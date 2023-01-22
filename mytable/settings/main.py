ce6f5724ff25908668db1401220f8a0655ea6120
poss03251 <mpossamaim@gmail.com>
2023-01-22 21:13:15 +0100

    for deployment test

--- mytable/settings/main.py
+++ mytable/settings/main.py
@@ -4,22 +4,19 @@ import os
 from dotenv import load_dotenv
 
 # LOADING CUSTOM ENV VARS
-ENV_TYPE = os.getenv("ENV_TYPE", default="venv")
+ENV_TYPE = os.getenv("ENV_TYPE", default="master")
 if ENV_TYPE == "local":
     paths = [
-        "./.env",
         "./envs/django/.local-configs.env",
         "./envs/postgres/.local-configs.env",
     ]
 elif ENV_TYPE == "venv":
     paths = [
-        "./.env",
         "./envs/django/.venv-configs.env",
         "./envs/postgres/.venv-configs.env",
     ]
 elif ENV_TYPE == "master":
     paths = [
-        "./.env",
         "./envs/django/.configs.env",
         "./envs/postgres/.configs.env",
     ]
