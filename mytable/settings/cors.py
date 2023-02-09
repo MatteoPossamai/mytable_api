b924c30ac25a0f2aa38fd690b2df4db7350ff3ec
poss03251 <mpossamaim@gmail.com>
2023-02-09 14:21:15 +0100

    allowed cors headers to be able to get requests

--- mytable/settings/cors.py
+++ mytable/settings/cors.py
@@ -10,9 +10,30 @@ if os.getenv('CORS_ALLOWED_ORIGINS') is not None:
 elif os.getenv('CORS_ALLOWED_ORIGIN_REGEXES') is not None:
     CORS_ALLOWED_ORIGIN_REGEXES = eval(os.getenv('CORS_ALLOWED_ORIGIN_REGEXES'))
 elif os.getenv("CORS_ALLOW_ALL_ORIGINS") is not None:
-    CORS_ALLOW_ALL_ORIGINS = eval(os.getenv("CORS_ALLOW_ALL_ORIGINS"))
+    CORS_ALLOW_ALL_ORIGINS = True
 
 #CORS_ALLOW_HEADERS = list(default_headers) + eval(os.getenv('CORS_ALLOW_HEADERS'))
 CORS_ALLOW_METHODS = eval(os.getenv('CORS_ALLOW_METHODS'))
+
+CORS_ALLOW_HEADERS = default_headers + [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Methods",
+    "HTTP_TOKEN",
+    "http_token",
+    "HTTP_AUTHORIZATION",
+    "HTTP_ORIGIN",
+    "TOKEN",
+    "token"
+]
 
 CORS_ORIGIN_ALLOW_ALL = True
