0fc2339fc2c08e1c421f2919c1cb813996e77d66
poss03251 <mpossamaim@gmail.com>
2023-02-16 17:17:02 +0100

    new order model and migrations

--- order/models/order.py
+++ order/models/order.py
@@ -4,8 +4,8 @@ from restaurant.models.restaurant import Restaurant
 
 class Order(models.Model):
     payment_method = models.CharField(max_length=255)
-    payment_status = models.CharField(max_length=255)
-    order_status = models.CharField(max_length=255) # 1: Submitted, 2: Received, 3: Preparing, 4: Ready, 5: Delivered
+    paid = models.BooleanField(default=False)
+    delivered = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
     note = models.TextField(blank=True, null=True)
 
