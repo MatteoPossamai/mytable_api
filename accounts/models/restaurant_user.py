796bfba1b0eb04348ab65d36a9bfda1061f6d03d
poss03251 <mpossamaim@gmail.com>
2023-02-19 21:27:56 +0100

    last modification to user data in tables

--- accounts/models/restaurant_user.py
+++ accounts/models/restaurant_user.py
@@ -5,6 +5,8 @@ from accounts.models import BaseUser
 class RestaurantUser(BaseUser):
     level = models.IntegerField(default=0)
     stripe_customer_id = models.CharField(max_length=100)
+    has_used_free_trial = models.BooleanField(default=False)
+    referral_code = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.email
