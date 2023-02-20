f847d30440922b9d930025683f42c378bfe95a9f
poss03251 <mpossamaim@gmail.com>
2023-02-20 14:50:18 +0100

    changed constrains to avoid conflicts

--- restaurant/models/item.py
+++ restaurant/models/item.py
@@ -4,7 +4,7 @@ from .category import Category
 
 class Item(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=False)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     iconId = models.IntegerField()
