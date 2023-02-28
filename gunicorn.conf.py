277121e903bf14a0679ad2fd33152621f679e5ac
poss03251 <mpossamaim@gmail.com>
2023-02-28 07:46:59 +0100

    added log configurations

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,21 @@
 from multiprocessing import cpu_count
 from os import environ
 
+# Server Socket
 bind = '0.0.0.0:' + environ.get('PORT', '443')
 max_requests = 1000
 workers = cpu_count()
 timeout = 30
+
+# Logging
+loglevel ='info'
+accessformat = '%(t)s %(h)s %(l)s %(r)s %(s)s %(b)s %(f)s %(a)s'
+accesslog = '-'
+errorlog = '-'
+
+# SSL
+"""
+certfile = 'server.crt'
+keyfile = 'server.key'
+ssl_version = 2
+"""
